5559b2f64a5dec980386022fd5e088fe5970e278
Maksym_Sharov <sharom01@kits.kfplc.com>
2023-05-09 13:35:50 +0300

    Remove .js files from server source

--- src/store.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var store = {
-    refreshToken: '',
-    accessToken: '',
-    cursor: null
-};
-exports.default = store;
